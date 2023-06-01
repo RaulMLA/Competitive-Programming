347f260a20799745e5a48566e72ce56ef9ee83da
RaulMLA <raulmanzanerolopezaguado@gmail.com>
2023-06-02 01:19:57 +0200

    Create 677A_Vanya-and-Fence.py

---
+++ Codeforces/677A_Vanya-and-Fence.py
@@ -0,0 +1,11 @@
+n, h = map(int, input().split())
+heights = list(map(int, input().split()))
+count = 0
+
+for height in heights:
+    if height > h:
+        count += 2
+    else:
+        count += 1
+
+print(count)
