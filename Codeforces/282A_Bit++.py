29f60734db38f2fe8e72190c0c34b4941c2d84e6
RaulMLA <raulmanzanerolopezaguado@gmail.com>
2023-06-10 00:41:21 +0200

    Update 282A_Bit++.py

--- Codeforces/282A_Bit++.py
+++ Codeforces/282A_Bit++.py
@@ -4,10 +4,9 @@ x = 0
 while n != 0:
     statement = input()
     if statement in ['X++', '++X']:
-        x += 1
-        n -= 1
-    if statement in ['X--', '--X']:
+        x += 1      
+    elif statement in ['X--', '--X']:
         x -= 1
-        n -= 1
+    n -= 1
 
 print(x)
